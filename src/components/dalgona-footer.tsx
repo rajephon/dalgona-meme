a9ec2727fc2449086ae5d21ec1d188cb38f4be23
rajephon <rajephon@gmail.com>
2021-11-04 01:57:59 +0900

    footer donation 버튼 위치 변경, predeploy npm으로 변경

--- src/components/dalgona-footer.tsx
+++ src/components/dalgona-footer.tsx
@@ -16,13 +16,7 @@ const DalgonaFooter = () => {
         <Container id="footer" maxWidth={"lg"}
                    sx={{marginTop: "15px"}}>
             <Grid container sx={{borderTop: "1px solid #eee", paddingTop: "15px"}}>
-                <Grid item xs={5} md={3}>
-                    <a href="https://thankyou-for.com/creator/rajephon" target="_blank">
-                        <img height="40" style={{border: 0, height: "40px"}}
-                             src="https://de56jrhz7aye2.cloudfront.net/banner/black.svg" alt="thank you for button"/>
-                    </a>
-                </Grid>
-                <Grid item xs={7} md={9} sx={{paddingLeft:"10px"}}>
+                <Grid item xs={7} md={9}>
                     <Typography component="div" sx={{flexGrow: 1}}>
                         Developed by <a href="https://rajephon.dev" target="_blank">rajephon</a> / <a
                         href="https://github.com/rajephon/dalgona-meme" target="_blank"><GitHubIcon
@@ -30,6 +24,12 @@ const DalgonaFooter = () => {
                         <a href="https://github.com/rajephon/dalgona-meme/issues" target="_blank">Report
                             bug &amp; issue</a>
                     </Typography>
+                </Grid>
+                <Grid item xs={5} md={3}>
+                    <a href="https://thankyou-for.com/creator/rajephon" target="_blank">
+                        <img height="40" style={{border: 0, height: "40px"}}
+                             src="https://de56jrhz7aye2.cloudfront.net/banner/black.svg" alt="thank you for button"/>
+                    </a>
                 </Grid>
             </Grid>
         </Container>
