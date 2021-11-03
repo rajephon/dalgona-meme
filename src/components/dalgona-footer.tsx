d7ff33f223a874f654acabd17a67942523802d58
rajephon <rajephon@gmail.com>
2021-11-04 02:36:01 +0900

    npm ci 임시 제거

--- src/components/dalgona-footer.tsx
+++ src/components/dalgona-footer.tsx
@@ -9,24 +9,21 @@ import Grid from "@mui/material/Grid";
 
 
 const DalgonaFooter = () => {
-    // return <Container id="footer" maxWidth={"lg"} sx={{marginTop: "15px", paddingTop: "15px", borderTop:"1px solid #eee"}}>
-
-    // </Container>;
     return <div>
         <Container id="footer" maxWidth={"lg"}
                    sx={{marginTop: "15px"}}>
             <Grid container sx={{borderTop: "1px solid #eee", paddingTop: "15px"}}>
                 <Grid item xs={7} md={9}>
                     <Typography component="div" sx={{flexGrow: 1}}>
-                        Developed by <a href="https://rajephon.dev" target="_blank">rajephon</a> / <a
-                        href="https://github.com/rajephon/dalgona-meme" target="_blank"><GitHubIcon
+                        Developed by <a href="https://rajephon.dev" target="_blank" rel="noreferrer">rajephon</a> / <a
+                        href="https://github.com/rajephon/dalgona-meme" target="_blank" rel="noreferrer"><GitHubIcon
                         fontSize="inherit"/> Github</a><br/>
-                        <a href="https://github.com/rajephon/dalgona-meme/issues" target="_blank">Report
+                        <a href="https://github.com/rajephon/dalgona-meme/issues" target="_blank" rel="noreferrer">Report
                             bug &amp; issue</a>
                     </Typography>
                 </Grid>
                 <Grid item xs={5} md={3}>
-                    <a href="https://thankyou-for.com/creator/rajephon" target="_blank">
+                    <a href="https://thankyou-for.com/creator/rajephon" target="_blank" rel="noreferrer">
                         <img height="40" style={{border: 0, height: "40px"}}
                              src="https://de56jrhz7aye2.cloudfront.net/banner/black.svg" alt="thank you for button"/>
                     </a>
