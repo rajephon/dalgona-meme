222dfe8054507b49040e4baf1cda2a39bf456a20
rajephon <rajephon@gmail.com>
2021-11-04 01:34:11 +0900

    도네 추가

--- src/components/dalgona-footer.tsx
+++ src/components/dalgona-footer.tsx
@@ -5,16 +5,35 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Grid from "@mui/material/Grid";
 
 
 const DalgonaFooter = () => {
-    return <Container id="footer" maxWidth={"lg"} sx={{marginTop: "15px", paddingTop: "15px", borderTop:"1px solid #eee"}}>
-        <Typography component="div" sx={{flexGrow: 1}}>
-            Developed by <a href="https://rajephon.dev" target="_blank">rajephon</a> / <a href="https://github.com/rajephon/dalgona-meme" target="_blank"><GitHubIcon fontSize="inherit" /> Github</a><br />
-            <a href="https://github.com/rajephon/dalgona-meme/issues" target="_blank">Report bug &amp; issue</a>
-        </Typography>
+    // return <Container id="footer" maxWidth={"lg"} sx={{marginTop: "15px", paddingTop: "15px", borderTop:"1px solid #eee"}}>
 
-    </Container>;
+    // </Container>;
+    return <div>
+        <Container id="footer" maxWidth={"lg"}
+                   sx={{marginTop: "15px"}}>
+            <Grid container sx={{borderTop: "1px solid #eee", paddingTop: "15px"}}>
+                <Grid item xs={5} md={3}>
+                    <a href="https://thankyou-for.com/creator/rajephon" target="_blank">
+                        <img height="40" style={{border: 0, height: "40px"}}
+                             src="https://de56jrhz7aye2.cloudfront.net/banner/black.svg" alt="thank you for button"/>
+                    </a>
+                </Grid>
+                <Grid item xs={7} md={9} sx={{paddingLeft:"10px"}}>
+                    <Typography component="div" sx={{flexGrow: 1}}>
+                        Developed by <a href="https://rajephon.dev" target="_blank">rajephon</a> / <a
+                        href="https://github.com/rajephon/dalgona-meme" target="_blank"><GitHubIcon
+                        fontSize="inherit"/> Github</a><br/>
+                        <a href="https://github.com/rajephon/dalgona-meme/issues" target="_blank">Report
+                            bug &amp; issue</a>
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Container>
+    </div>
 }
 
 export default DalgonaFooter;
